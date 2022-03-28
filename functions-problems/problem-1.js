793058caf2d73c3911f028af63187fe7d87ad0e3
Erika <erikaangela4@gmail.com>
2022-03-28 08:34:23 -0700

    Morning session. Wrote instructions. Needs to install node.js.

---
+++ functions-problems/problem-1.js
@@ -0,0 +1,17 @@
+// Write a JavaScript function that accepts a string as a parameter and find the longest word within the string.
+
+/* Steps
+1. Define a JavaScript function that accepts a string as a parameter.
+2. Declare and initialize a variable, testString.
+3. Use split() to create a new array of individual words of the accepted string, split between spaces.
+4. Loop through the new array, comparing each word's length to each other.
+5. Return the value of the index with the greatest length.
+*/
+
+let testString = 'This is the given test string'
+
+function findLongestWord(str) {
+  str.split(" ");
+}
+
+console.log(findLongestWord(testString));
