dd1fae500fb122c01b8648aec40e49bf5cee6e5c
Erika <erikaangela4@gmail.com>
2022-03-29 08:17:42 -0700

    Morning session. New, simpler direction.

--- functions-problems/problem-1.js
+++ functions-problems/problem-1.js
@@ -3,21 +3,22 @@
 /* Steps
 1. Define a JavaScript function that accepts a string as a parameter.
 2. Declare and initialize a variable, testString.
-3. Use split() to create a new array of individual words of testString, split between spaces.
-4. Loop through the new array, comparing each word's length to each other.
-5. Return the value of the index with the greatest length.
+3. Use split() to create a new array of individual words of testString, split between spaces, called splitString.
+4. Loop through splitString, comparing each word's length to each other.
+5. Return the value at the index with the greatest length.
 */
 
 let testString = 'This is the given test string'
 
 function findLongestWord(str) {
-  str.split(" ");
-  let numberOfLetters = [];
-  for (i = 0; i < str.length; i++) {
-    numberOfLetters.push(i.length);
+  let splitString = str.split(" ");
+  let longestWord = ""; // longestWord starting at length 0
+  for (let i = 0; i < splitString.length; i++) {
+    if (splitString[i].length > longestWord.length) {
+      longestWord = splitString[i];
+    }
   }
-  return numberOfLetters;
+  return longestWord;
 }
 
-let test = findLongestWord(testString);
-console.log(test);
+console.log(findLongestWord(testString)); // prints 'string'
