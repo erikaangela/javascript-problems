f2b5e0a557b66eb748790bbfe793e694235f992b
Erika <erikaangela4@gmail.com>
2022-03-30 20:21:55 -0700

    Evening session. Started DOM, Problem 1.

---
+++ dom-problems/problem-1.js
@@ -0,0 +1,18 @@
+// Create a basic HTML page with a P element and a button on it.
+
+// Then, write a JavaScript function to set the background color of that paragraph using an "onclick" event added to a button in the UI.
+
+<!DOCTYPE html>
+
+<html>
+
+  <head></head>
+
+  <body>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam convallis ut sem vel fermentum. Sed volutpat nisl ornare finibus maximus. Etiam viverra imperdiet nisl nec bibendum. Aliquam egestas imperdiet magna, nec semper eros sodales sed. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In rhoncus fermentum sem consequat venenatis.</p>
+    <button></button>
+
+  </body>
+
+</html>
