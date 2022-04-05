50285132e230adbf73ed1034200f61a1c79c3166
Erika <erikaangela4@gmail.com>
2022-04-05 12:51:43 -0700

    Noon session. Starting Arrays, Problem 3.

---
+++ array-problems/problem-3.js
@@ -0,0 +1,10 @@
+/* Define a function that takes in an array of numbers/integers and returns the sorted version of that array, greatest to least.
+
+Example array: const myArray = [-3,8,7,6,5,-4,3,2,1];
+
+Hint: To solve this, look up the JavaScript array sort method, and pay close attention to how it uses it's return values of 1, 0, -1 to sort items.
+*/
+
+/* Steps
+1. 
+*/
