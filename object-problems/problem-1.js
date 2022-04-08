cf969328a7c3ce711f2d0c0acd824da2d65ba175
Erika <erikaangela4@gmail.com>
2022-04-08 08:00:53 -0700

    Morning session. New object-problems folder and finished Objects, Problem 1.

---
+++ object-problems/problem-1.js
@@ -0,0 +1,24 @@
+/* Create a sample object, then write a JavaScript function that deletes the property of an object passed to it.
+
+Your JavaScript function should take in two parameters, the object and the key/property you want to delete.
+*/
+
+/* Steps
+1. Create an arbitrary object with one or more properties.
+2. Define a JavaScript function that takes in two parameters, the object and key/property.
+4. Within the function, remove the passed object's property.
+*/
+
+const dayObject = {
+  weather: 'rainy',
+  currentTimeOfDay: 6,
+  color: 'yellow',
+  numberOfErrands: 5
+}
+
+function removeObjectProperty (objectName, propName) {
+  delete objectName.[propName];
+}
+
+removeObjectProperty(dayObject, 'color');
+console.log(dayObject);
