8c566d3b7acd83af3da84882df7f5c8e1104c40b
Erika <erikaangela4@gmail.com>
2022-04-14 11:56:25 -0700

    Noon session. Added alternative solution using a for loop for Recursion, Problem 2.

--- recursion-problems/problem-2.js
+++ recursion-problems/problem-2.js
@@ -19,10 +19,15 @@ function findTheSumOf (arr) {
 
 console.log(findTheSumOf(arrayOfIntegers));
 
-
-// function findTheSumOf (arr) {
-//   if (arr.length === 1) {
-//     return arr[0];
-//   }
-//   return arr[arr.length] + findTheSumOf(arr.slice(1, ));
-// }
+// Alternative solution using for loop to find sum and product
+// https://www.w3resource.com/javascript-exercises/javascript-array-exercise-12.php#:~:text=Write%20a%20JavaScript%20program%20to,of%20an%20array%20of%20integers.&text=ES6%20Version%3A,array%5Bi%5D%3B%20%7D%20console.
+// const arrayOfIntegers = [44, 19, 22, 27, 0, 9];
+// let startingValueSum = 0;
+// let startingValuePro = 1;
+// let i;
+// for (i = 0; i < array.length; i += 1)
+//    {
+//     startingValueSum += array[i];
+//     startingValuePro *= array[i];
+//     }
+// console.log(`Sum : ${startingValueSum} Product :  ${startingValuePro}`);
